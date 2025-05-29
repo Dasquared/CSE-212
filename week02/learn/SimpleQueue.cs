96d6b3aaeb817db1e0694a3c4c33f591fa76e5a6
dasquared <156029602+Dasquared@users.noreply.github.com>
2025-05-28 23:25:53 -0500

    changues

---
+++ week02/learn/SimpleQueue.cs
@@ -0,0 +1,83 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+public class SimpleQueue
+{
+    public static void Run()
+    {
+        // Test Cases
+
+        // Test 1
+        // Scenario: Enqueue one value and then Dequeue it.
+        // Expected Result: It should display 100
+
+        Console.WriteLine("Test 1");
+        var queue = new SimpleQueue();
+        queue.Enqueue(100);
+        var value = queue.Dequeue();
+        Console.WriteLine(value);
+        // Defect(s) Found: when printing the program the index was set in the second position, 
+        // so the unique item was not found
+
+        Console.WriteLine("------------");
+
+        // Test 2
+        // Scenario: Enqueue multiple values and then Dequeue all of them
+        // Expected Result: It should display 200, then 300, then 400 in that order
+        Console.WriteLine("Test 2");
+        queue = new SimpleQueue();
+        queue.Enqueue(200);
+        queue.Enqueue(300);
+        queue.Enqueue(400);
+        value = queue.Dequeue();
+        Console.WriteLine(value);
+        value = queue.Dequeue();
+        Console.WriteLine(value);
+        value = queue.Dequeue();
+        Console.WriteLine(value);
+        // Defect(s) Found: the enqueu item was inserted in the front of the list giving the idea of a stack
+
+        Console.WriteLine("------------");
+
+        // Test 3
+        // Scenario: Dequeue from an empty Queue
+        // Expected Result: An exception should be raised
+        Console.WriteLine("Test 3");
+        queue = new SimpleQueue();
+        try
+        {
+            queue.Dequeue();
+            Console.WriteLine("Oops ... This shouldn't have worked.");
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("I got the exception as expected.");
+        }
+        // Defect(s) Found: Not defects found, the message was printed right!
+    }
+
+    private readonly List<int> _queue = new();
+
+    /// <summary>
+    /// Enqueue the value provided into the queue
+    /// </summary>
+    /// <param name="value">Integer value to add to the queue</param>
+    private void Enqueue(int value)
+    {
+        _queue.Add(value);
+    }
+
+    /// <summary>
+    /// Dequeue the next value and return it
+    /// </summary>
+    /// <exception cref="IndexOutOfRangeException">If queue is empty</exception>
+    /// <returns>First integer in the queue</returns>
+    private int Dequeue()
+    {
+        if (_queue.Count <= 0)
+            throw new IndexOutOfRangeException();
+
+        var value = _queue[0];
+        _queue.RemoveAt(0);
+        return value;
+    }
+}
